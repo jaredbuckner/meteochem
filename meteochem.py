95e3af4b25e539d59032fce34a685e49258a5921
Jared Buckner <jaredbuckner@gmail.com>
2021-03-25 17:55:35 -0400

    Add enthalpy data

--- meteochem.py
+++ meteochem.py
@@ -83,70 +83,73 @@ elements = {
            'a': 65.38}
     }
 
-molecules = {'H2': {'T3':  13.80},
-             'N2': {'T3':  63.15},
-             'O2': {'T3':  54.36},
-             'F2': {'T3':  53.48},
+# T3: Triple point temperature (K), where available.  Melting point is used when unavailable.
+# Hf: Standard enthalpy of formation (kJ/mol)
 
-             'He': {'T3':   2.18},
-             'Ne': {'T3':  24.56},
-             'Ar': {'T3':  83.81},
+molecules = {'H2': {'T3':  13.80, 'Hf': 0.0},
+             'N2': {'T3':  63.15, 'Hf': 0.0},
+             'O2': {'T3':  54.36, 'Hf': 0.0},
+             'F2': {'T3':  53.48, 'Hf': 0.0},
+
+             'He': {'T3':   2.18, 'Hf': 0.0},
+             'Ne': {'T3':  24.56, 'Hf': 0.0},
+             'Ar': {'T3':  83.81, 'Hf': 0.0},
              
-             'C':  {'T3':  3915},
-             'Al': {'T3':  933.47},
-             'Mg': {'T3':  923},
-             'Si': {'T3':  1687},
-             'Ti': {'T3':  1941},
-             'Cr': {'T3':  2180},
-             'Mn': {'T3':  1519},
-             'Fe': {'T3':  1811},
-             'Co': {'T3':  1768},
-             'Ni': {'T3':  1728},
-             'Cu': {'T3':  1358},
-             'Zn': {'T3':   693},
+             'C':  {'T3':  3915, 'Hf': 0.0},
+             'Al': {'T3':  933.47, 'Hf': 0.0},
+             'Mg': {'T3':  923, 'Hf': 0.0},
+             'Si': {'T3':  1687, 'Hf': 0.0},
+             'Ti': {'T3':  1941, 'Hf': 0.0},
+             'Cr': {'T3':  2180, 'Hf': 0.0},
+             'Mn': {'T3':  1519, 'Hf': 0.0},
+             'Fe': {'T3':  1811, 'Hf': 0.0},
+             'Co': {'T3':  1768, 'Hf': 0.0},
+             'Ni': {'T3':  1728, 'Hf': 0.0},
+             'Cu': {'T3':  1358, 'Hf': 0.0},
+             'Zn': {'T3':   693, 'Hf': 0.0},
 
-             'CH4': {'T3':  90.67},
-             'NH3': {'T3': 195.4},
-             'H2O': {'T3': 273.16},
-             'HF':  {'T3': 190},
-             'H2S': {'T3': 187.66},
-             'HCl': {'T3': 161.15},
+             'CH4': {'T3':  90.67, 'Hf': -74.6},
+             'NH3': {'T3': 195.4,  'Hf': -46},
+             'H2O': {'T3': 273.16, 'Hf': -291.83},
+             'HF':  {'T3': 190,    'Hf': -13.66},
+             'H2S': {'T3': 187.66, 'Hf': -21},
+             'HCl': {'T3': 161.15, 'Hf': -92.31},
 
-             'CO':    {'T3':  67.9},
-             'CO2':   {'T3': 216.58},
-             'P2O5':  {'T3': 613},
-             'SO2':   {'T3': 197.64},
+             'CO':    {'T3':  67.9,  'Hf': -110.5},
+             'CO2':   {'T3': 216.58, 'Hf': -393.5},
+             'P2O5':  {'T3': 613,    'Hf': -1452},
+             'SO2':   {'T3': 197.64, 'Hf': -296.81},
 
-             'H3PO4': {'T3': 314},
+             'H3PO4': {'T3': 314, 'Hf': -1271.7},
 
-             'MgO':   {'T3': 3125},
-             'Al2O3': {'T3': 2345},
-             'SiO2':  {'T3': 3220},
-             'CaO':   {'T3': 2886},
-             'TiO2':  {'T3': 2116},
-             'Cr2O3': {'T3': 2708},
-             'MnO':   {'T3': 2218},
-             'Fe3O4': {'T3': 1870},
-             'Co3O4': {'T3': 1168},
-             'NiO':   {'T3': 2228},
-             'Cu2O':  {'T3': 1505},
-             'ZnO':   {'T3': 2247},
+             'MgO':   {'T3': 3125, 'Hf': -601.6},
+             'Al2O3': {'T3': 2345, 'Hf': -1675.7},
+             'SiO2':  {'T3': 3220, 'Hf': -911},
+             'CaO':   {'T3': 2886, 'Hf': -635},
+             'TiO2':  {'T3': 2116, 'Hf': -945},
+             'Cr2O3': {'T3': 2708, 'Hf': -1128},
+             'MnO':   {'T3': 2218, 'Hf': -385},
+             'Fe3O4': {'T3': 1870, 'Hf': -1120.89},
+             'CoO':   {'T3': 2206, 'Hf': -237.74},
+             'NiO':   {'T3': 2228, 'Hf': -240},
+             'Cu2O':  {'T3': 1505, 'Hf': -170},
+             'ZnO':   {'T3': 2247, 'Hf': -350.5},
              
-             'MnS':   {'T3': 1983},
-             'FeS':   {'T3': 1467},
-             'CoS':   {'T3': 1468},
-             'NiS':   {'T3': 1070},
-             'Cu2S':  {'T3': 1400},
-             'ZnS':   {'T3': 2120},
+             'MnS':   {'T3': 1983, 'Hf': -345.72},
+             'FeS':   {'T3': 1467, 'Hf': -101.67},
+             'CoS':   {'T3': 1468, 'Hf': -190},  # Estimated?
+             'NiS':   {'T3': 1070, 'Hf': -87.86},
+             'Cu2S':  {'T3': 1400, 'Hf': -120},  # Estimated, badly?
+             'ZnS':   {'T3': 2120, 'Hf': -204.6},
 
-             'NaF':   {'T3': 1266},
-             'NaCl':  {'T3': 1073},
-             'Mg2F':  {'T3': 1536},
-             'Mg2Cl': {'T3':  987},
-             'CaF2':  {'T3': 1691},
-             'CaCl2': {'T3': 1046},
-             'KF':    {'T3': 1131},
-             'KCl':   {'T3': 1040}
+             'NaF':   {'T3': 1266, 'Hf': -573.6},
+             'NaCl':  {'T3': 1073, 'Hf': -411.12},
+             'Mg2F':  {'T3': 1536, 'Hf': -1124.2},
+             'Mg2Cl': {'T3':  987, 'Hf': -641.1},
+             'CaF2':  {'T3': 1691, 'Hf': -1225.91},
+             'CaCl2': {'T3': 1046, 'Hf': -795.42},
+             'KF':    {'T3': 1131, 'Hf': -568.61},
+             'KCl':   {'T3': 1040, 'Hf': -436}
              }
 
 
@@ -154,6 +157,7 @@ molecules = {'H2': {'T3':  13.80},
 def moldata(mol):
     components = {}
     a = 0
+    n = 0
     z = 0
     
     s = mol
@@ -180,12 +184,14 @@ def moldata(mol):
             components[atom] = components[atom] + howmany
         else:
             components[atom] = howmany
-
+            
         a += elements[atom]['a'] * howmany
+        n += howmany
         z += elements[atom]['z'] * howmany
 
     return {'components': components,
             'a': a,
+            'n': n,
             'z': z}
 
 enames = ()
@@ -199,7 +205,7 @@ for name, data in molecules.items():
     print(f'Constructing data sheet for {name} ...')
     data.update(moldata(name))
 
-mollist = sorted(molecules.keys(), key=lambda k: molecules[k]['a'], reverse=True)
+mollist = sorted(molecules.keys(), key=lambda k: molecules[k]['Hf'] / molecules[k]['n'])
 
 for mol in mollist:
     print(f'{mol} => {molecules[mol]!r}')
@@ -243,7 +249,7 @@ def nebula_molecules(elemsperdraw, mintw=0, mint3=0):
 # d <-> sqrt(1/T**4)   or  d <-> 1/T**2
 # (d1 * T1**2 == d2 * T2**2)   or   T2**2 == d1 / d2 * T1**2
                     
-cutoff = random.uniform(7,35)
+cutoff = random.uniform(0,35)
 au = random.uniform(2.06,3.27)
 #cutoff = random.uniform(0, 35)
 #cutoff = 0
